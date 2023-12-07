c2047901c3610f17a364e622b33d34af7a0d46c3
SharpDevOps10 <td090833@gmail.com>
2023-12-07 23:14:06 +0200

    migration #4: added product model
    closes #3

---
+++ prisma/migrations/20231207210944_added_product/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
