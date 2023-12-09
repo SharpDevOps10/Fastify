705f3f523ed23ef2a3b9a6e37ae2b73f2bb9a84c
SharpDevOps10 <td090833@gmail.com>
2023-12-09 22:15:49 +0200

    feature #13: added auth decorator
    closes #13

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,24 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
+import fastifyJwt from '@fastify/jwt';
 import userRoute from './modules/user/user.route';
 import { userSchemas } from './modules/user/user.schema';
 
 const server = Fastify();
+
+server.register(fastifyJwt, {
+  secret: process.env.SECRET as string,
+});
+
+server.decorate(
+  'auth',
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+    } catch (e) {
+      return reply.send(e);
+    }
+  }
+);
 
 async function main () {
   for (const schema of userSchemas) {
@@ -11,9 +27,10 @@ async function main () {
 
   server.register(userRoute, { prefix: 'api/users' });
   try {
-    await server.listen({ port: 3000, host: '0.0.0.0' });
+    const port = Number(process.env.PORT);
+    await server.listen({ port, host: '0.0.0.0' });
 
-    console.log('Server ready at http://localhost:3000');
+    console.log(`Server ready at http://localhost:${port}`);
   } catch (e) {
     console.error(e);
     process.exit(1);
