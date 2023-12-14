09da5f4c4c50671afdb7df78b87a8eefc1e8fa82
SharpDevOps10 <td090833@gmail.com>
2023-12-14 18:56:33 +0200

    feature #19: added products get all endpoint
    closes #19

--- src/modules/products/product.route.ts
+++ src/modules/products/product.route.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify';
-import { createProductHandler } from './product.controller';
+import { createProductHandler, getProductsHandler } from './product.controller';
 import { $ref } from './product.schema';
 
 const productRoute = async (server: FastifyInstance) => {
@@ -12,6 +12,14 @@ const productRoute = async (server: FastifyInstance) => {
       },
     },
   }, createProductHandler);
+
+  server.get('/', {
+    schema: {
+      response: {
+        200: $ref('productsResponseSchema'),
+      },
+    },
+  }, getProductsHandler);
 };
 
 export default productRoute;
--- src/modules/products/product.service.ts
+++ src/modules/products/product.service.ts
@@ -4,3 +4,22 @@ import prisma from '../../utils/prisma';
 export const createProduct = async (data: CreateProductInput & { ownerId: number }) => {
   return prisma.product.create({ data });
 };
+
+export const getProducts = async () => {
+  return prisma.product.findMany({
+    select: {
+      content: true,
+      title: true,
+      price: true,
+      id: true,
+      createdAt: true,
+      updatedAt: true,
+      owner: {
+        select: {
+          name: true,
+          id: true,
+        },
+      },
+    },
+  });
+};
